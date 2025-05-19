7b84ca2a5338fac0e36a86d43482111d136eadf6
luis felipe galleguillos <luisfelipegalleguillos@hotmail.com>
2025-05-19 14:30:34 -0400

    creating og image and twitter image

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -13,8 +13,29 @@ const geistMono = Geist_Mono({
 });
 
 export const metadata: Metadata = {
-  title: " Desenvolvedor | Luís Felipe Galleguillos Campos",
+  title: "Desenvolvedor | Luís Felipe Galleguillos Campos",
   description: "Minha página pessoal, contatos, portfólio e outras coisas.",
+  openGraph:{
+    title: 'Desenvolvedor | Luís Felipe Galleguillos Campos',
+    description: 'Minha página pessoal, contatos, portfólio e outras coisas.',
+    url: 'https://galleguillos.com.br/',
+    siteName: 'Galleguillos',
+    images: [
+      {
+        url: 'https://galleguillos.com.br/og.jpg',
+        width: 1200,
+        height: 630,
+        alt: 'Preview image for Galleguillos',
+      },
+    ],
+    type: 'website',
+  },
+  twitter: {
+    card: 'summary_large_image',
+    title: 'Desenvolvedor | Luís Felipe Galleguillos Campos',
+    description: 'Minha página pessoal, contatos, portfólio e outras coisas.',
+    images: ['https://galleguillos.com.br/og.jpg'],
+  },
 };
 
 export default function RootLayout({
